8ee9bd5075ee00a49500e51f25d7b3ce5987af01
Ecnama <amance@famille-graindorge.fr>
2025-03-12 16:18:09 +0100

    Clean everything before coding

--- build-pyplanet/run.sh
+++ build-pyplanet/run.sh
@@ -5,14 +5,13 @@ echo "PYPLANET_HOME: ${PYPLANET_HOME}"
 
 # func: keep pyplanet running
 run_pyplanet() {
-  cd $PYPLANET_HOME
-  while :
-  do
-    echo "PyPlanet starting..."
-    python3 manage.py start
-    echo "PyPlanet exited, restarting in 15 seconds..."
-    sleep 15
-  done
+	cd $PYPLANET_HOME
+	while :; do
+		echo "PyPlanet starting..."
+		python3 manage.py start
+		echo "PyPlanet exited, restarting in 15 seconds..."
+		sleep 15
+	done
 }
 
 # run services
